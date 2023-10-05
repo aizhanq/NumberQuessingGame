32dbcbf846da7aa1ce899b11310e7e39582f68b2
Aizhan <aizhankarasartova@gmail.com>
2023-10-05 20:54:36 +0600

    Добавьте файлы проекта.

---
+++ NumberQuessingGame/Program.cs
@@ -0,0 +1,29 @@
+﻿Random r = new Random();
+
+int winNum = r.Next(1, 100);
+
+bool win = false;
+
+do
+{
+    Console.Write("Quess a number inbetween 0 and 100: ");
+    string a = Console.ReadLine();
+    int i = int.Parse(a);
+
+    if (i > winNum)
+    {
+        Console.WriteLine("To high! Quess lower...");
+    }
+    else if (i < winNum)
+    {
+        Console.WriteLine("To low! Quess higher...");
+    }
+    else if (i == winNum)
+    {
+        Console.WriteLine("YOU WIN!");
+        win = true;
+    }
+} while (win == false);
+
+Console.WriteLine("Thank you for playing game!");
+Console.WriteLine("Press any ley to finish.");
